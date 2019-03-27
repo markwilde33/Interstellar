a20fe72c1eb3018ee7a082723a3c252c782c860b
Mark Wilde <markwilde29@gmail.com>
2019-03-27 16:58:46 +0000

    Created the view for the get_bugs.html page

--- bugs/views.py
+++ bugs/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.contrib.auth.models import User
+from .models import Bug, TicketComment
+from .forms import TicketForm, CommentForm
 
-# Create your views here.
+
+@login_required()
+def get_bugs(request):
+    """
+    Create a view that will return a
+    list of tickets that were published prior to'now'
+    and render them to the 'tickets.html' template
+    """
+    bugs = Bug.objects.filter(most_recent_update__lte=timezone.now()
+                                ).order_by('-most_recent_update')
+    return render(request, "get_bugs.html", {'bugs': bugs})
