67418c1987967ab9cb1fee6dbf65eed92bd3b45b
Mark Wilde <markwilde29@gmail.com>
2019-03-28 10:55:48 +0000

    Minor color changes made to footer link and text. Added the view and logic for the get_bugs page and the url. Created the get_bugs page which was added in the previous commit.

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from .views import get_bugs
+from .views import get_bugs, bug_view
 
 
 urlpatterns = [
     url(r'^$', get_bugs, name='get_bugs'),
-    
+    url(r'^(?P<pk>\d+)/$', bug_view, name='bug_view'),
     ]
--- bugs/views.py
+++ bugs/views.py
@@ -16,3 +16,32 @@ def get_bugs(request):
     bugs = Bug.objects.filter(most_recent_update__lte=timezone.now()
                                 ).order_by('-most_recent_update')
     return render(request, "get_bugs.html", {'bugs': bugs})
+    
+    
+@login_required() 
+def bug_view(request, pk):
+    """
+    Create a view that will return a single ticket
+    object based on the ticket ID and render it
+    to the 'ticket_view.html' template, return a
+    comment form for users to add a comment,
+    and return all previously added comments,
+    or return a 404 error if ticket is not found
+    """
+    bug = get_object_or_404(Bug, pk=pk)
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST, request.FILES)
+        
+        if comment_form.is_valid():
+            ticketComment = comment_form.save(commit=False)
+            ticketComment.ticket = bug
+            ticketComment.user = request.user
+            ticketComment.save()
+            return redirect(reverse('bug_view', kwargs={'pk': pk}))
+            
+    else:
+        comment_form = CommentForm()
+        bug_comments = TicketComment.objects.filter(ticket__pk=bug.pk)
+        bug.views += 1
+        bug.save()
+        return render(request, 'bug_view.html', {'bug':bug, 'bug_comments':bug_comments, 'comment_form':comment_form})
